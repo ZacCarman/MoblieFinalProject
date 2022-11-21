a08410e1ed033b74c5a12d6e1944c73ae9fe9f3b
AlexanderPapachristu <55894236+AlexanderPapachristu@users.noreply.github.com>
2022-11-20 20:24:53 -0500

    Added some functionality for cart

--- MoblieFinalProject/ProjectFolder/app/src/main/java/com/example/mobliefinalproject/cartActivity.java
+++ MoblieFinalProject/ProjectFolder/app/src/main/java/com/example/mobliefinalproject/cartActivity.java
@@ -2,13 +2,59 @@ package com.example.mobliefinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class cartActivity extends AppCompatActivity {
+    Button itemButton, cartButton, signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        //***********NAV BAR************************
+        itemButton = findViewById(R.id.itemButton);
+        cartButton = findViewById(R.id.cartButton);
+        signInButton = findViewById(R.id.signInButton);
+        signUpButton = findViewById(R.id.signUpButton);
+
+
+        itemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(getApplicationContext(), itemActivity.class);
+                startActivity(intent);
+            }
+        });
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(getApplicationContext(), cartActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(getApplicationContext(), signUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(getApplicationContext(), signInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //******************************END OF NAV BAR*****************************
+
+        String [] cart;
+
     }
 }
